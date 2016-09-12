ac7c19cd6f0c48879f6c12cb6d9ce851ea39ac12
Laurent Bo <misterbl@hotmail.com>
2016-09-12 18:30:41 +0000

    only names shorter than 12 letters

--- directory.rb
+++ directory.rb
@@ -23,7 +23,7 @@ def print_header
 end
 
 def print(students)
-    students.each_with_index{|student, index| puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("D","d")}
+    students.each_with_index{|student, index| puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12}
   
 end
 
