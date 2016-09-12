5e79eaff04313f820df598b4df17ba02c674a157
Laurent Bo <misterbl@hotmail.com>
2016-09-12 18:29:23 +0000

    only names starting with'd' or 'D'

--- directory.rb
+++ directory.rb
@@ -1,22 +1,3 @@
-def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
-end
-
-def print(students)
-    students.each_with_index do |student, index|
-    puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-end
-
-def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
-end
-
-#nothing happens until we call the methods
-
-
-
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
@@ -25,16 +6,34 @@ def input_students
     #get the first name
     name = gets.chomp
     #while the name is not empty
-    while !name.empty? do
+        while !name.empty? do
         #add the student hash to the array
         students << {name: name, cohort: :november}
         puts "Now we have #{students.count} students"
         #get another name from the user
         name = gets.chomp
-    end
+        end
         #return the arrary of students
         students
-    end
+end
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
+
+def print(students)
+    students.each_with_index{|student, index| puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("D","d")}
+  
+end
+
+
+def print_footer(students)
+    puts "Overall, we have #{students.count} great students"
+end
+
+#nothing happens until we call the methods
+
 students = input_students    
 print_header
 print(students)
