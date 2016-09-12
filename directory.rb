3322cde7f58b11c92e9daccc11012e37cdaaf8fb
Laurent Bo <misterbl@hotmail.com>
2016-09-12 18:56:59 +0000

    more information about the students

--- directory.rb
+++ directory.rb
@@ -5,10 +5,22 @@ def input_students
     students = []
     #get the first name
     name = gets.chomp
+    #get the hobbies
+    puts "what are your hobbies?"
+    hobbies = gets.chomp
+    #get country of birth
+    puts "What is your country of birth?"
+    country_of_birth = gets.chomp
+    #get their height
+    puts "What is your height?"
+    height = gets.chomp
+    #get their weight
+    puts "What is your weight?"
+    weight = gets.chomp
     #while the name is not empty
         while !name.empty? do
         #add the student hash to the array
-        students << {name: name, cohort: :november}
+        students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: country_of_birth, height: height, weight: weight}
         puts "Now we have #{students.count} students"
         #get another name from the user
         name = gets.chomp
@@ -23,7 +35,7 @@ def print_header
 end
 
 def print(students)
-    students.each_with_index{|student, index| puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)" until student[:name].length < 12}
+    students.each_with_index{|student, index| puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort, hobbies: #{student[:hobbies]}, born in #{student[:country_of_birth]}, height: #{student[:height]}, weight: #{student[:weight]})"}
   
 end
 
