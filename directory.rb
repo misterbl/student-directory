fb48a996829eacf482bc42a8f5967fdcba52025d
Laurent Bo <misterbl@hotmail.com>
2016-09-12 13:33:49 +0000

    print_header, print, print_footer methods

--- directory.rb
+++ directory.rb
@@ -13,11 +13,22 @@ students = [
     "Norman Bates" 
 ]
 
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-     puts student
- end
-# finally, we print the total number of students
-print "Overall, we have #{students.count} great students"
+def print(names)
+    names.each do |name|
+    puts name
+    end
+end
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
