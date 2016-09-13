45bbc374455c15f16397a3fc6cf2e7ff5cbbec03
Laurent Bo <misterbl@hotmail.com>
2016-09-13 17:41:08 +0000

    input with more infos working properly

--- directory.rb
+++ directory.rb
@@ -1,47 +1,58 @@
 def input_students
     puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    #create an empty array
-    students = []
-    #get the first name
-    name = gets.chomp
-    #get the hobbies
-    puts "what are your hobbies?"
-    hobbies = gets.chomp
-    #get country of birth
-    puts "What is your country of birth?"
-    country_of_birth = gets.chomp
-    #get their height
-    puts "What is your height?"
-    height = gets.chomp
-    #get their weight
-    puts "What is your weight?"
-    weight = gets.chomp
-    #while the name is not empty
-        while !name.empty? do
-        #add the student hash to the array
-        students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: country_of_birth, height: height, weight: weight}
-        puts "Now we have #{students.count} students"
-        #get another name from the user
-        name = gets.chomp
-        end
-        #return the arrary of students
-        students
+   puts "To finish, just hit return twice"
+   #create an empty array
+   students = []
+   #get the first name
+   loop do
+   puts "What is the student\'s name?"
+   name = gets.chomp
+    if name.empty? 
+           break 
+       end
+   #get their cohort
+   puts "What cohort are they on?"
+   cohort = gets.chomp
+   #get the hobbies
+   puts "what are their hobbies?"
+   hobbies = gets.chomp
+   #get country of birth
+   puts "What is their country of birth?"
+   country = gets.chomp
+   #get their height
+   puts "What is their height?"
+   height = gets.chomp
+   #get their weight
+   puts "What is their weight?"
+   weight = gets.chomp
+   #while the name is not empty
+  
+       #add the student hash to the array
+       students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height, weight: weight}
+       puts "Now we have #{students.count} students"
+       #get another name from the user
+     
+      
+       end
+       #return the arrary of students
+
+       students
 end
 
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+   puts "The students of Villains Academy"
+   puts "-------------"
 end
 
 def print(students)
-    students.each_with_index{|student, index| puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort, hobbies: #{student[:hobbies]}, born in #{student[:country_of_birth]}, height: #{student[:height]}, weight: #{student[:weight]})"}
-  
+   students.each_with_index do |student, index|
+     puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort): Country of birth: #{student[:country]}. Hobbies: #{student[:hobbies]}. Height: #{student[:height]}. Weight:#{student[:weight]}".center(20)
+   end
 end
 
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+   puts "Overall, we have #{students.count} great students"
 end
 
 #nothing happens until we call the methods
