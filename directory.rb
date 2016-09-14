668d6a93e89b4d6d738ee80a22e4b16f7a36010e
Laurent Bo <misterbl@hotmail.com>
2016-09-14 19:31:55 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -1,7 +1,6 @@
+  @students = []
 def input_students
-       #create an empty array
-   students = []
-   #get the first name and cohort
+       #get the first name and cohort
    loop do
    puts "Enter the student\'s name, press 'enter' and then type in the cohort"
    puts "To finish, just hit return twice"
@@ -39,53 +38,76 @@ def input_students
    #while the name is not empty
   
        #add the student hash to the array
-       students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height, weight: weight}
-       if students.count > 0
-        puts "Now we have #{students.count} students"
-    end
-       #get another name from the user
-     
-      
-       end
-       #return the arrary of students
-
-       students
+       @students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height, weight: weight}
+       if @students.count > 0
+        puts "Now we have #{@students.count} students"
+        end
+   end
 end
 
 def print_header 
-    if input_students.count > 0
-   puts "The students of Villains Academy"
+    puts "The students of Villains Academy"
    puts "-------------"
-   end
 end
 
-def print(students)
+
+def print_student_list
     puts "Which cohort would you like see the list of? If all, type: All"
     which_cohort = gets.chomp
-     students.each_with_index do |student, index|
-    if which_cohort == "All"
-      puts "#{index + 1}.#{student[:name]}. Cohort: #{student[:cohort]}. Country of birth: #{student[:country]}. Hobbies: #{student[:hobbies]}. Height: #{student[:height]}. Weight:#{student[:weight]}".center(150) if students.count > 0
+     @students.each_with_index do |student, index|
+    if which_cohort.downcase == "All".downcase
+      puts "#{index + 1}.#{student[:name]}. Cohort: #{student[:cohort]}. Country of birth: #{student[:country]}. Hobbies: #{student[:hobbies]}. Height: #{student[:height]}. Weight:#{student[:weight]}".center(150) if @students.count > 0
       elsif which_cohort == student[:cohort]
-    students.map{|c| c[:cohort]}
-   puts "#{index + 1}.#{student[:name]}. Cohort: #{student[:cohort]}. Country of birth: #{student[:country]}. Hobbies: #{student[:hobbies]}. Height: #{student[:height]}. Weight:#{student[:weight]}".center(150) if students.count > 0
+    @students.map{|c| c[:cohort]}
+   puts "#{index + 1}.#{student[:name]}. Cohort: #{student[:cohort]}. Country of birth: #{student[:country]}. Hobbies: #{student[:hobbies]}. Height: #{student[:height]}. Weight:#{student[:weight]}".center(150) if @students.count > 0
    end
    end
 end
 
 
-def print_footer(students)
-    if students.count > 0
-    if students.count > 1
-   puts "Overall, we have #{students.count} great students"
-   else
-   puts "Overall, we have #{students.count} great student" 
+def print_footer
+    if @students.count > 0
+        if @students.count > 1
+            puts "Overall, we have #{@students.count} great students"
+        else
+            puts "Overall, we have #{@students.count} great student" 
    end
    end
 end
 
+def print_menu
+      # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+    print_header
+    print_student_list
+    print_footer
+end
+
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students 
+    when "9"
+        exit # this will cause the program to terminate
+    else
+        puts "I don't know what you meant, try again"
+    end
+  end
+  
+def interactive_menu
+   loop do
+    print_menu
+    process(gets.chomp)
+   
+end
+end
+    
 #nothing happens until we call the methods
-
-students = input_students    
-print_header
-print(students)
-print_footer(students)
+interactive_menu
