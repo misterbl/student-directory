f1f0d954f46fd16f18dccde6bb3c16811bd6d5f8
Laurent Bo <misterbl@hotmail.com>
2016-09-15 13:53:34 +0000

    code refactored

--- directory.rb
+++ directory.rb
@@ -1,68 +1,4 @@
   @students = []
-def input_students
-    if ARGV.nil?
-    puts "Do you want to load the students information from a file?"
-    load_or_not = STDIN.gets.chomp
-    if load_or_not.downcase == "yes"
-        puts "Enter the filename (including the extension)"
-        filename = STDIN.gets.chomp
-        file = File.open(filename, "r")
-        file.readlines.each do |line|
-        name, cohort, hobbies, country, height, weight = line.chomp.split(',')
-        @students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height, weight: weight}
-        end
-        file.close
-    elsif load_or_not.downcase == "no"
-    loop do
-    puts "Enter the student\'s name, press 'enter' and then type in the cohort"
-    puts "To finish, just hit return twice"
-    name = STDIN.gets.strip
-    if name.empty? 
-           break 
-    end
-    cohort = STDIN.gets.strip
-    if cohort.empty?
-        cohort = "Cohort not enter"
-    end
-    puts "what are their hobbies?"
-    hobbies = STDIN.gets.strip
-     if hobbies.empty?
-       hobbies = "Not entered"
-    end
-   #get country of birth
-    puts "What is their country of birth?"
-    country = gets.strip
-    if country.empty?
-       country = "Not entered"
-    end
-   #get their height
-    puts "What is their height?"
-    height = STDIN.gets.strip
-    if height.empty?
-       height = "Not entered"
-    end
-   #get their weight
-    puts "What is their weight?"
-    weight = STDIN.gets.strip
-    if weight.empty?
-       weight = "Not entered"
-    end
-       #add the student hash to the array
-    @students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height, weight: weight}
-    if @students.count > 0
-    puts "Now we have #{@students.count} students"
-    end
-   end
-   end
-   end
-   else try_load_students
-end
-
-def print_header 
-    puts "The students of Villains Academy"
-   puts "-------------"
-end
-
 
 def print_student_list
     puts "Which cohort would you like see the list of? If all, type: All"
@@ -77,17 +13,6 @@ def print_student_list
    end
 end
 
-
-def print_footer
-    if @students.count > 0
-        if @students.count > 1
-            puts "Overall, we have #{@students.count} great students"
-        else
-            puts "Overall, we have #{@students.count} great student" 
-   end
-   end
-end
-
 def print_menu
       # 1. print the menu and ask the user what to do
     puts "1. Input the students"
@@ -97,9 +22,16 @@ def print_menu
 end
 
 def show_students
-    print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
     print_student_list
-    print_footer
+    if @students.count > 0
+        if @students.count > 1
+            puts "Overall, we have #{@students.count} great students"
+        else
+            puts "Overall, we have #{@students.count} great student" 
+        end
+    end
 end
 
 def process(selection)
@@ -121,8 +53,7 @@ def interactive_menu
    loop do
     print_menu
     process(STDIN.gets.chomp)
-   
-end
+    end
 end
 
 def save_students
